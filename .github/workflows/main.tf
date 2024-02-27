8e35d495dc20f1dfbfe4c287d5355dc078d49c8a
Warkann2 <99244680+Warkann2@users.noreply.github.com>
2024-02-27 15:00:18 +0100

    Create main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,12 @@
+{
+  "clientId": "76cb4104-aa88-4ab0-a4be-ec6c5d133109",
+  "clientSecret": "xVc8Q~f2Y-n5mAH1Jrf1nEy21_HSn39JpPYgkbaH",
+  "subscriptionId": "2cd9031c-913b-4de9-89d5-ff2f8b779574",
+  "tenantId": "38e72bba-3c22-4382-9323-ac1612931297",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
